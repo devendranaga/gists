d601b1d41cdf30e018c1682c3ae26ba2ab6b01b9
Devendra <devendra.aaru@gmail.com>
2016-03-13 11:26:39 -0400

    modify hash example based on the latest mbedtls code

--- hash_Test.c
+++ hash_Test.c
@@ -1,9 +1,10 @@
-#include "polarssl/config.h"
-#include "polarssl/platform.h"
-#include "polarssl/md5.h"
+#include "mbedtls/config.h"
+#include "mbedtls/platform.h"
+#include "mbedtls/md5.h"
 #include <stdio.h>
 #include <stdint.h>
-          
+#include <string.h>
+
 int digest_print(uint8_t *str, int len)
 {
     int i;
@@ -13,6 +14,8 @@ int digest_print(uint8_t *str, int len)
     }
 
     printf("\n");
+
+    return 0;
 }
 
 int main(int argc, char **argv)
@@ -20,15 +23,15 @@ int main(int argc, char **argv)
     int i;
     uint8_t digest[16];
               
-  if (argc != 2) {
-      printf("%s <string>\n", argv[0]);
-      return -1;
-  }
+    if (argc != 2) {
+        printf("%s <string>\n", argv[0]);
+        return -1;
+    }
               
-  md5(argv[1], strlen(argv[1]), digest);
+    mbedtls_md5(argv[1], strlen(argv[1]), digest);
               
-  digest_print(digest, sizeof(digest));
+    digest_print(digest, sizeof(digest));
               
-  return 0;
+    return 0;
 
 }
