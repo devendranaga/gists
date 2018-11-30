41e87f0470a721a329cc48212da2f3f888b4d76e
DevNaga <devendra.aaru@gmail.com>
2018-11-29 22:11:21 -0500

    update

---
+++ truncate.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+long filesize(char *filename)
+{
+    struct stat st;
+    int ret;
+
+    ret = stat(filename, &st);
+    if (ret < 0) {
+        return -1;
+    }
+
+    return st.st_size;
+}
+
+int main(int argc, char **argv)
+{
+    long size;
+    int fd;
+    int ret;
+
+    if (argc != 2) {
+        fprintf(stderr, "<%s> filename\n", argv[0]);
+        return -1;
+    }
+
+    ret = truncate(argv[1], 1024 * 1024);
+    if (ret < 0) {
+        fprintf(stderr, "failed to truncate file to 1M error :%s \n", strerror(errno));
+        return -1;
+    }
+
+    fprintf(stderr, "truncated file %s\n", argv[1]);
+
+    fprintf(stderr, "unlinking %s\n", argv[1]);
+
+    ret = unlink(argv[1]);
+    if (ret < 0) {
+        fprintf(stderr, "failed to unlink %s\n", strerror(errno));
+        return -1;
+    }
+
+    fd = open(argv[1], O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd < 0) {
+        fprintf(stderr, "failed to open file %s\n", argv[1]);
+        return -1;
+    }
+
+    ret = ftruncate(fd, 1024 * 1024);
+    if (ret < 0) {
+        fprintf(stderr, "failed to ftruncate file to 1M error: %s\n", strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
