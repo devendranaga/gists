8f086a9285db8f0dcdc463e35f20076b44ea646b
DevNaga <devendra.aaru@gmail.com>
2018-12-06 10:17:05 -0500

    update

---
+++ recv.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SAMPLE_SERVER_CONN 10
+
+int main(int argc, char **argv)
+{
+    int ret;
+    int sock;
+    int conn;
+    int set_reuse = 1;
+    struct sockaddr_in remote;
+    socklen_t remote_len;
+    char buf[1000];
+
+    if (argc != 3) {
+        fprintf(stderr, "%s [ip] [port]\n", argv[0]);
+        return -1;
+    }
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("failed to socket\n");
+        return -1;
+    }
+
+    ret = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &set_reuse, sizeof(set_reuse));
+    if (ret < 0) {
+        perror("failed to setsockopt\n");
+        close(sock);
+        return -1;
+    }
+
+    remote.sin_family = AF_INET;
+    remote.sin_addr.s_addr = inet_addr(argv[1]);
+    remote.sin_port = htons(atoi(argv[2]));
+
+    ret = bind(sock, (struct sockaddr *)&remote, sizeof(remote));
+    if (ret < 0) {
+        perror("failed to bind\n");
+        close(sock);
+        return -1;
+    }
+
+    ret = listen(sock, SAMPLE_SERVER_CONN);
+    if (ret < 0) {
+        perror("failed to listen\n");
+        close(sock);
+        return -1;
+    }
+
+    remote_len = sizeof(struct sockaddr_in);
+
+    conn = accept(sock, (struct sockaddr *)&remote, &remote_len);
+    if (conn < 0) {
+        perror("failed to accept\n");
+        close(sock);
+        return -1;
+    }
+
+    printf("new client %d\n", conn);
+
+    memset(buf, 0, sizeof(buf));
+
+    printf("waiting for the data ... \n");
+    ret = recv(conn, buf, sizeof(buf), 0);
+    if (ret <= 0) {
+        printf("failed to recv\n");
+        close(conn);
+        return -1;
+    }
+
+    printf("received %s \n", buf);
+
+    close(conn);
+
+    return 0;
+}
