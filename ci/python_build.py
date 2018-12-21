01a3980da14fcdf4a504f95a0e855f9a483724a6
MADMAX <devendra.aaru@gmail.com>
2018-12-20 22:37:35 -0800

    Update python_build.py

--- ci/python_build.py
+++ ci/python_build.py
@@ -12,11 +12,11 @@ exceptions = ['lib.c', 'md_list.c', 'hash_Test.c', 'cipher_list.c', 'dlopen.c', 
 for file in glob.glob('*.c'):
     print(file)
     if file not in exceptions:
-        x = os.system('gcc ' + file + ' -ldl -rdynamic -pthread -lm')
+        x = os.system('gcc ' + file + ' -ldl -rdynamic -pthread -lm -lrt')
         if x != 0:
             exit(1)
 
-x = os.system('gcc lib.c dlopen.c -ldl -rdynamic')
+x = os.system('gcc lib.c dlopen.c -ldl -rdynamic -lrt')
 if x != 0:
     exit(1)
 
