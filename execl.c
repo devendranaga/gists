75cc5f1d4b3ad1ff6954760a32733beeaf015d62
Dev <devendra.aaru@gmail.com>
2018-12-22 00:04:43 -0600

    update

---
+++ execl.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+    int ret;
+    pid_t pid;
+
+    if (argc != 3) {
+        fprintf(stderr, "<%s> command command-opt ..\n", argv[0]);
+        return -1;
+    }
+
+    pid = fork();
+    if (pid != 0) {
+        ret = execl(argv[1], argv[1], argv[2], NULL);
+        if (ret < 0) {
+            fprintf(stderr, "failed to execl %s\n", strerror(errno));
+            return -1;
+        }
+    }
+}
+
